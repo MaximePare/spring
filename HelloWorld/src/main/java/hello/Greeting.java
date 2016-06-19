5b18ee5d962dd1c0fa94f240be5cceafbba5b28f
Maxime Pare <maxime.pare.4@ulaval.ca>
2016-06-19 14:43:55 -0400

    ajout du projet

---
+++ HelloWorld/src/main/java/hello/Greeting.java
@@ -0,0 +1,22 @@
+package hello;
+
+/**
+ * Created by Maxime on 2016-06-19.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
